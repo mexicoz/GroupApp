c1326bd8dd5ef4dd949cb36d26146bb5afc41099
OgurtsovDmitry <defaultikom@gmail.com>
2022-12-23 17:08:16 +0200

    controllers

---
+++ GroupApp/Controllers/RaceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GroupApp.Controllers
+{
+	public class RaceController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
