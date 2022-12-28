8951f0c9daf74d9214d9da6967f8e6fc126ed61e
OgurtsovDmitry <defaultikom@gmail.com>
2022-12-28 15:39:01 +0200

    create race

--- GroupApp/Controllers/RaceController.cs
+++ GroupApp/Controllers/RaceController.cs
@@ -1,29 +1,42 @@
-﻿using GroupApp.Data;
-using GroupApp.Interfaces;
+﻿using GroupApp.Interfaces;
 using GroupApp.Models;
+using GroupApp.Repository;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace GroupApp.Controllers
 {
 	public class RaceController : Controller
 	{
-        private readonly IRaceRepository raceRepository;
+        private readonly IRaceRepository _raceRepository;
 
         public RaceController(IRaceRepository raceRepository)
 		{
-            this.raceRepository = raceRepository;
+            _raceRepository = raceRepository;
         }
         public async Task<IActionResult> Index()
 		{
-			IEnumerable<Race> races = await raceRepository.GetAllRaces();
+			IEnumerable<Race> races = await _raceRepository.GetAllRaces();
 
 			return View(races);
 		}
         public async Task<IActionResult> Detail(int id)
         {
-            Race race = await raceRepository.GetAllRacesById(id);
+            Race race = await _raceRepository.GetAllRacesById(id);
             return View(race);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Race race)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(race);
+            }
+            _raceRepository.Add(race);
+            return RedirectToAction("Index");
         }
     }
 }
