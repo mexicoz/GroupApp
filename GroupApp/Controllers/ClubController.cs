e71bde93a0c155d38f48a7973fc9295336cbfbfb
OgurtsovDmitry <defaultikom@gmail.com>
2022-12-28 14:29:48 +0200

    create club

--- GroupApp/Controllers/ClubController.cs
+++ GroupApp/Controllers/ClubController.cs
@@ -1,8 +1,6 @@
-﻿using GroupApp.Data;
-using GroupApp.Interfaces;
+﻿using GroupApp.Interfaces;
 using GroupApp.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace GroupApp.Controllers
 {
@@ -24,6 +22,20 @@ namespace GroupApp.Controllers
 		{
 			Club club = await _clubRepository.GetClubById(id);
 			return View(club);
+		}
+		public IActionResult Create()
+		{
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> Create(Club club)
+		{
+			if(!ModelState.IsValid)
+			{
+				return View(club);
+			}
+			_clubRepository.Add(club);
+			return RedirectToAction("Index");
 		}
 	} 
 }
