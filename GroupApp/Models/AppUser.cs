b0a6293ab9ed4cb658baeb61d8b587d7068f19f2
OgurtsovDmitry <defaultikom@gmail.com>
2023-01-02 13:52:13 +0200

    identity

--- GroupApp/Models/AppUser.cs
+++ GroupApp/Models/AppUser.cs
@@ -10,6 +10,7 @@ namespace GroupApp.Models
 
 		[ForeignKey("Address")]
 		public int AddressId { get; set; }
+		public Address Address { get; set; }
 		public ICollection<Club> Clubs { get; set; }
 		public ICollection<Race> Races { get; set; }
 	}
