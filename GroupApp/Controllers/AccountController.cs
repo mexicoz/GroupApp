32e25575fc9d632d79ab8521cf6811a5a2676471
OgurtsovDmitry <defaultikom@gmail.com>
2023-01-02 14:49:03 +0200

    Login

---
+++ GroupApp/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using Azure.Core;
+using GroupApp.Data;
+using GroupApp.Models;
+using GroupApp.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GroupApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        public IActionResult Login()
+        {
+            var responce = new LoginViewModel();
+            return View(responce);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel login)
+        {
+            if(!ModelState.IsValid) return View(login);
+
+            var user = await _userManager.FindByEmailAsync(login.Email);
+            if(user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, login.Password);
+                if (passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, login.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Race");
+                    }
+                }
+                TempData["Error"] = "Wrong credentials.";
+                return View(login);
+            }
+            TempData["Error"] = "Wrong credentials.";
+            return View(login);
+        }
+    }
+}
---
+++ GroupApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GroupApp.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "Email address")]
+        [Required(ErrorMessage = "Email address is required")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
