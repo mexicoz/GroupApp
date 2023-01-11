e8d837ed19f3b2435d741bb4ad94932e415d7899
OgurtsovDmitry <defaultikom@gmail.com>
2023-01-11 12:51:44 +0200

    geo

--- GroupApp/Controllers/HomeController.cs
+++ GroupApp/Controllers/HomeController.cs
@@ -1,21 +1,55 @@
-﻿using GroupApp.Models;
+﻿using GroupApp.Helpers;
+using GroupApp.Interfaces;
+using GroupApp.Models;
+using GroupApp.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Globalization;
+using System.Net;
 
 namespace GroupApp.Controllers
 {
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+        private readonly IClubRepository _clubRepository;
 
-		public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IClubRepository clubRepository)
 		{
 			_logger = logger;
-		}
+            _clubRepository = clubRepository;
+        }
+		private const string URL_TOKEN = "https://ipinfo.io?token=86f7867313caaf";
 
-		public IActionResult Index()
+        public async Task<IActionResult> Index()
 		{
-			return View();
+			var ipInfo = new IPInfo();
+			var homeVM = new HomeViewModel();
+			try
+			{
+				var info = new WebClient().DownloadString(URL_TOKEN);
+				ipInfo = JsonConvert.DeserializeObject<IPInfo>(info);
+				RegionInfo myRI = new RegionInfo(ipInfo.Country);
+				ipInfo.Country = myRI.EnglishName;
+				homeVM.City = ipInfo.City;
+				homeVM.State = ipInfo.Region;
+
+				if(homeVM.City != null)
+				{
+					homeVM.Clubs = await _clubRepository.GetClubByCity(homeVM.City);
+				}
+				else
+				{
+					homeVM.Clubs = null;
+				}
+				return View(homeVM);
+            }
+			catch(Exception ex)
+			{
+                homeVM.Clubs = null;
+            }
+			return View(homeVM);
 		}
 
 		public IActionResult Privacy()
